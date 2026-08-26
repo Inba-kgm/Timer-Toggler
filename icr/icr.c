b676cabf4be794a40c96fe14f6ed7d0deb62e7ef
Inba-kgm <inbavanans7484@gmail.com>
2026-08-26 20:02:36 +0530

    Updated Task3 with Issue

--- icr/icr.c
+++ icr/icr.c
@@ -12,6 +12,7 @@ volatile uint8_t *pind = (uint8_t*)0x29;
 volatile uint8_t *pinb = (uint8_t*)0x23;
 volatile uint8_t *pinc = (uint8_t*)0x26;
 
+
 volatile uint8_t *timer1_ctrl_A = (uint8_t*)0x80;
 volatile uint8_t *timer1_ctrl_B = (uint8_t*)0x81;
 volatile uint8_t *timer1_flag = (uint8_t*)0x36;
@@ -20,15 +21,33 @@ volatile uint8_t *timer1_counter_H = (uint8_t*)0x85;
 volatile uint16_t *timer1_output_compare = (uint16_t*)0x88;
 volatile uint8_t *timer1_output_compare_L = (uint8_t*)0x88;
 volatile uint8_t *timer1_mask = (uint8_t*)0x6F;
+volatile uint8_t *sreg = (uint8_t*)0x5F;
+volatile uint16_t *input_capture_1 = (uint16_t*)0x86;
+
+void __attribute__((signal,used,externally_visible))__vector_10(){
+	*timer1_ctrl_B ^= (1<<6);
+	*timer1_flag |=(1<<5);
+	if(*timer1_ctrl_B & (1<<6)){
+	*portb|= (1<<5);
+	}
+	else{
+	*portb &= ~(1<<5);
+	}
+}
 
 int main(){
-    *timer1_ctrl_A |= (1<<6);
-    *timer1_ctrl_B |= (1<<3)|(1<<2);
-    *timer1_counter = 0;
-    *timer1_output_compare = 62499;
-    *timer1_mask |= (1<<1);
-    *timer1_flag |= (1<<1);
+
+    *ddrb &= ~(1<<0);
+	*ddrb |= (1<<5);
+	*portb |= (1<<0);
+    *timer1_ctrl_A = 0;
+	*timer1_counter = 0;
+    *sreg |= (1<<7);
+	*timer1_flag |= (1<<5);
+	*timer1_mask |= (1<<5);
+	*timer1_ctrl_B |= (1<<2);
+	*timer1_ctrl_B &= ~(1<<6);
     while(1){
-        //pass
+
     }
 }
